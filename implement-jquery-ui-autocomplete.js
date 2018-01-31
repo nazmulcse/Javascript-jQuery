cd1e8532ba3567fd7752b2c0ac73e3ff28410f45
Nazmul Hasan <nazmul.sbpgc@gmail.com>
2018-01-31 14:29:53 +0600

    Update implement-jquery-ui-autocomplete.js

--- implement-jquery-ui-autocomplete.js
+++ implement-jquery-ui-autocomplete.js
@@ -1,4 +1,5 @@
 1. For Remote binding (data comes from server with every on keyup)
+----------------------------------------------------------------------
 
 $(function () {
         $("#id").autocomplete({        // id is id of input control
@@ -33,3 +34,17 @@ $(function () {
             }
       echo json_encode($json_data);
     
+2. For Local Binding :
+-----------------------------------------
+var AutocompleteData = '<?php echo json_encode($data)?>';    // Add php array data to javascript.
+var AutocompleteData = JSON.parse(AutocompleteData);      // parse json to javascript array
+$("#id").autocomplete({
+    source: AutocompleteData,   // Json parsed data
+
+    minLength: 2,
+    select: function (event, ui) {
+        $("#id").val(ui.item.id);
+    }
+});
+
+// Note : For this autocomplete must be added "jquery-ui.js" file
